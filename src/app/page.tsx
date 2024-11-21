118b545215c300d803ff0f2fa3e881c213a0aaac
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-21 19:05:54 +0100

    add profile picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>Portfolio</h1>;
+  return (
+    <main>
+      <section>
+        <Image
+          src="/best.png"
+          width={500}
+          height={700}
+          alt="Picture of the slow chat application"
+          className="grayscale hover:grayscale-0 transition duration-300"
+        />
+        <h1>My Portfolio</h1>
+      </section>
+    </main>
+  );
 }
