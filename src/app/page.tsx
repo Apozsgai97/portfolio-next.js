2057f00b4d6454cf80ec0b2f86255fa2036672ce
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-15 18:39:53 +0100

    add about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { HeroSection, ProjectsSection } from "@/features";
+import { AboutMeSection, HeroSection, ProjectsSection } from "@/features";
 
 export default function Home() {
   return (
     <main className="pt-16">
       <HeroSection/>
+      <AboutMeSection/>
       <ProjectsSection/>
     </main>
   );
