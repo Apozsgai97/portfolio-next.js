580fdc6a38a4ebb28a64d170f3e9d4de685240fe
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-17 16:47:10 +0100

    add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function ContactPage() {
+  return (
+    <section className="pt-20 text-neutral-50 flex flex-col justify-center items-center h-full">
+      <h1 className="font-extrabold text-4xl mb-6 text-center">
+        Contact Me
+      </h1>
+      <form action=""></form>
+    </section>
+  );}
