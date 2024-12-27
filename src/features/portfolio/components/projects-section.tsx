3fde6f6382cf77ad4d7c42c7b2ec3cad30aeffd2
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-27 20:33:26 +0100

    change to daisyUi cards and make it responsive

--- src/features/portfolio/components/projects-section.tsx
+++ src/features/portfolio/components/projects-section.tsx
@@ -3,68 +3,77 @@ export function ProjectsSection() {
   return (
     <section className="border-b-4 border-pink-200 text-neutral-50 pb-12">
       <h2 className="text-4xl font-bold p-12 text-center">My Projects</h2>
-      <div>
-        <article className="border-2 border-pink-200 m-12 mx-36 flex justify-evenly items-center">
-          <Image
-            src="/slow-chat.png"
-            width={500}
-            height={300}
-            alt="Picture of the developer, Adrienn Pozsgai"
-            className="m-4"
-          />
-          <div className="p-10">
-            <h3 className="text-2xl font-bold pb-2">Slow Chat Application</h3>
-            <p className="leading-8 pr-12">
+      <div className="flex flex-col items-center gap-10">
+        <article className="card card-compact md:card-side bg-base-100 w-96 md:w-8/12 shadow-xl md:border-2 md:border-pink-200 rounded-none md:px-4">
+          <figure>
+            <Image
+              src="/includio.png"
+              width={500}
+              height={300}
+              alt="Picture of the developer, Adrienn Pozsgai"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-2xl">Includio</h2>
+            <p className="max-w-lg text-base leading-7">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad
               maiores quas culpa voluptatem vel ut aliquid praesentium explicabo
               iure impedit ullam eaque assumenda nulla vero dolores rem commodi,
               necessitatibus labore?
             </p>
-            <button className="btn btn-primary rounded-none hover:bg-pink-200 hover:text-zinc-800 mt-4">
-              Read more
-            </button>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary rounded-none hover:bg-pink-200 hover:text-zinc-800 mt-4">
+                Read more
+              </button>
+            </div>
           </div>
         </article>
-        <article className="border-2 border-pink-200 m-12 mx-36 flex justify-evenly items-center">
-          <Image
-            src="/includio.png"
-            width={500}
-            height={300}
-            alt="Picture of the developer, Adrienn Pozsgai"
-            className="m-4"
-          />
-          <div className="p-10">
-            <h3 className="text-2xl font-bold pb-2">Includio</h3>
-            <p className="leading-8 pr-12">
+        <article className="card card-compact md:card-side bg-base-100 w-96 md:w-8/12 shadow-xl  md:border-2 md:border-pink-200 rounded-none md:px-4">
+          <figure>
+            <Image
+              src="/slow-chat.png"
+              width={500}
+              height={300}
+              alt="Picture of the developer, Adrienn Pozsgai"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-2xl">Slow Chat Application</h2>
+            <p className="max-w-lg text-base leading-7">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad
               maiores quas culpa voluptatem vel ut aliquid praesentium explicabo
               iure impedit ullam eaque assumenda nulla vero dolores rem commodi,
               necessitatibus labore?
             </p>
-            <button className="btn btn-primary rounded-none hover:bg-pink-100 hover:text-zinc-800 mt-4">
-              Read more
-            </button>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary rounded-none hover:bg-pink-200 hover:text-zinc-800 mt-4">
+                Read more
+              </button>
+            </div>
           </div>
         </article>
-        <article className="border-2 border-pink-200 m-12 mx-36 flex justify-evenly items-center">
-          <Image
-            src="/voting-app.png"
-            width={500}
-            height={300}
-            alt="Picture of the developer, Adrienn Pozsgai"
-            className="m-4"
-          />
-          <div className="p-10">
-            <h3 className="text-2xl font-bold pb-2">Voting app</h3>
-            <p className="leading-8 pr-12">
+        <article className="card card-compact md:card-side bg-base-100 w-96 md:w-8/12 shadow-xl  md:border-2 md:border-pink-200 rounded-none md:px-4">
+          <figure>
+            <Image
+              src="/voting-app.png"
+              width={500}
+              height={300}
+              alt="Picture of the developer, Adrienn Pozsgai"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-2xl">Voting app</h2>
+            <p className="max-w-lg text-base leading-7">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad
               maiores quas culpa voluptatem vel ut aliquid praesentium explicabo
               iure impedit ullam eaque assumenda nulla vero dolores rem commodi,
               necessitatibus labore?
             </p>
-            <button className="btn btn-primary rounded-none hover:bg-pink-200 hover:text-zinc-800 mt-4">
-              Read more
-            </button>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary rounded-none hover:bg-pink-200 hover:text-zinc-800 mt-4">
+                Read more
+              </button>
+            </div>
           </div>
         </article>
       </div>
