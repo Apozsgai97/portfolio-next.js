34588f93ab5d09436f72e017866fbe1c442907bc
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-01-07 19:46:43 +0100

    add description to the projects

--- src/features/portfolio/components/projects-section.tsx
+++ src/features/portfolio/components/projects-section.tsx
@@ -17,16 +17,41 @@ export function ProjectsSection() {
             />
           </figure>
           <div className="card-body">
-            <h2 className="card-title text-2xl">Includio</h2>
+            <h2 className="card-title text-2xl">Talent App</h2>
             <p className="max-w-lg text-base leading-7">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad
-              maiores quas culpa voluptatem vel ut aliquid praesentium explicabo
-              iure impedit ullam eaque assumenda nulla vero dolores rem commodi,
-              necessitatibus labore?
+              A platform showcasing developers for clients and sales. Developers
+              can add projects, skills, and education to their profiles. Admins
+              can manage developer scores, and a powerful search function allows
+              filtering by name, skill, or education.
             </p>
             <div className="card-actions justify-end">
               <button className="btn btn-primary rounded-none hover:bg-pink-200 hover:text-zinc-800 mt-4">
-                Read more
+                Check out on GitHub
+              </button>
+            </div>
+          </div>
+        </article>
+        <article className="card card-compact md:card-side bg-base-100 w-96 md:w-8/12 shadow-xl md:border-2 md:border-pink-200 rounded-none md:px-4">
+          <figure>
+            <Image
+              src="/includio.png"
+              width={500}
+              height={300}
+              alt="Picture of the developer, Adrienn Pozsgai"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-2xl">Includio</h2>
+            <p className="max-w-lg text-base leading-7">
+              A platform for businesses to assess and improve their
+              accessibility through detailed metrics and feedback. It enables
+              businesses to manage accessibility scores, view improvement
+              suggestions, and track progress, fostering inclusivity and
+              customer satisfaction.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary rounded-none hover:bg-pink-200 hover:text-zinc-800 mt-4">
+                Check out on GitHub
               </button>
             </div>
           </div>
@@ -43,14 +68,15 @@ export function ProjectsSection() {
           <div className="card-body">
             <h2 className="card-title text-2xl">Slow Chat Application</h2>
             <p className="max-w-lg text-base leading-7">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad
-              maiores quas culpa voluptatem vel ut aliquid praesentium explicabo
-              iure impedit ullam eaque assumenda nulla vero dolores rem commodi,
-              necessitatibus labore?
+              A messaging platform that encourages meaningful conversations by
+              limiting message interactions. With controlled fetching options
+              (daily and weekly), the app promotes slower, more thoughtful
+              communication, focusing on quality over speed in digital
+              conversations.
             </p>
             <div className="card-actions justify-end">
               <button className="btn btn-primary rounded-none hover:bg-pink-200 hover:text-zinc-800 mt-4">
-                Read more
+                Check out on GitHub
               </button>
             </div>
           </div>
@@ -67,14 +93,13 @@ export function ProjectsSection() {
           <div className="card-body">
             <h2 className="card-title text-2xl">Voting app</h2>
             <p className="max-w-lg text-base leading-7">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad
-              maiores quas culpa voluptatem vel ut aliquid praesentium explicabo
-              iure impedit ullam eaque assumenda nulla vero dolores rem commodi,
-              necessitatibus labore?
+              An interactive application for organizing and managing elections,
+              displaying results, and ensuring transparency with a robust
+              database for tracking votes and representatives.
             </p>
             <div className="card-actions justify-end">
               <button className="btn btn-primary rounded-none hover:bg-pink-200 hover:text-zinc-800 mt-4">
-                Read more
+                Check out on GitHub
               </button>
             </div>
           </div>
